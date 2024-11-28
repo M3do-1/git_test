d6e3e7411e2baea28fc59c76c8ac35613ebbdd2b
cico <92887800+M3do-1@users.noreply.github.com>
2024-11-28 08:10:44 -0500

    Applied old changes

--- main.js
+++ main.js
@@ -14,15 +14,4 @@ var myArray = ['Apple', 'Banana', 'Cherry'];
             document.getElementById('hi').style.background = "transparent";
             document.getElementById('hi').style.border = "0px solid";
             }
-
-                        // audio player from goole using Jquery
-                        $(document).ready(function () {
-                            audioPlayer = new oyoPlayer();
-                            $("body").append(audioPlayer);
-                            audioPlayer.addToPlaylist("Mr_wobot.mp3", "Mr. Wobot - I'm A Robot");
-                            // audioPlayer.addToPlaylist("audio/Drop Shot.mp3", "TaxiWars - Drop Shot");
-                            // audioPlayer.addToPlaylist("audio/How To Disappear Completely.mp3", "Radiohead - How To Disappear Completely");
-                            // audioPlayer.addToPlaylist("https://stream.player.arrow.nl/arrow", "Arrow Classic Rock");
-                            audioPlayer.setSourceIndex(1);
-                            audioPlayer.setNotification("Push Play to start");
-                        });
+            
